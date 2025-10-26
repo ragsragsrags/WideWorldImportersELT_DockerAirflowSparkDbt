e074711b5cd06772f58d5515ff47893681494afc
ragsragsrags <ruel_ragpa@yahoo.com>
2025-10-27 02:43:39 +0800

    Create LoadHistory and WarehouseHistory tables

---
+++ scripts/create_history_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `WideWorldImporters`.`LoadHistory`
+(
+  TableName STRING,
+  LoadDate DATETIME,
+  Status STRING,
+  Details STRING
+);
+
+CREATE TABLE `WideWorldImportersDW`.`WarehouseHistory`
+(
+  TableName STRING,
+  LoadDate DATETIME,
+  Status STRING,
+  Details STRING
+);
